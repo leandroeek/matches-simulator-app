6b3aba7a874deca4a5c57e09a21daa40cbb0a09c
leandroeek <leandroeek@hotmail.com>
2022-06-29 22:31:21 -0300

    Criado a domain, data classes e algumas funcoes

--- app/src/main/java/me/dio/simulator/ui/MainActivity.java
+++ app/src/main/java/me/dio/simulator/ui/MainActivity.java
@@ -1,10 +1,11 @@
-package me.dio.simulator;
+package me.dio.simulator.ui;
 
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import me.dio.simulator.databinding.ActivityMainBinding;
+import me.dio.simulator.domain.Team;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +16,21 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setupMatchesList();
+        setupMatchesRefresh();
+        setupFloatingActionButton();
 
+    }
+
+    private void setupMatchesList() {
+        //TODO: Listar as partidas, consumindo nossa API.
+    }
+
+    private void setupMatchesRefresh() {
+        //TODO: Atualizar as portidas na ação de swipe
+    }
+
+    private void setupFloatingActionButton() {
+        //TODO: Criar evento de clique e simulação de partidas.
     }
 }
