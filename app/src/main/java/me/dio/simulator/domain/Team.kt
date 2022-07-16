ab970efcd84f26c601fa56a31de7443ee71168f4
leandroeek <leandroeek@hotmail.com>
2022-07-15 23:21:58 -0300

    Finalizando app

--- app/src/main/java/me/dio/simulator/domain/Team.kt
+++ app/src/main/java/me/dio/simulator/domain/Team.kt
@@ -1,7 +1,16 @@
 package me.dio.simulator.domain
 
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Team (
+    @SerializedName("nome")
     val name: String,
+    @SerializedName("estrelas")
     val stars: Int,
-    val image: String
-)
+    @SerializedName("imagem")
+    val image: String,
+    var score: Int?
+): Parcelable
